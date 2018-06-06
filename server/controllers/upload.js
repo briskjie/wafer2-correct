9ba90584504481846db47f00cc6e56eec8b4ff41
xiangxuchen <xiangxuchen@sohu-inc.com>
2018-06-06 18:09:38 +0800

    dev

--- server/controllers/upload.js
+++ server/controllers/upload.js
@@ -4,14 +4,11 @@ const { uploader } = require('../qcloud')
 module.exports = async ctx => {
   // 获取上传之后的结果
   // 具体可以查看：
-  const data = await uploader(ctx.req)
-  var imageInfo = {
-    imgUrl: '',
-    imgKey: ''
-  }
-  imageInfo.imgUrl = data.imgUrl
-  imageInfo.imgKey = data.imgKey
+  const {imgUrl,imgKey}= await uploader(ctx.req)
+  var imageInfo={}
+  imageInfo.imgUrl = imgUrl
+  imageInfo.imgKey = imgKey
   await mysql("album").insert(imageInfo)//将上传图片保存到数据库
   // var res = await mysql("album").where('id', 1)
-  ctx.state.data = data
+  ctx.state.data = imageInfo
 }
