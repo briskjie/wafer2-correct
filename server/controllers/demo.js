72b7b2791759309528f4f85f98028dad19096aab
xiangxuchen <xiangxuchen@sohu-inc.com>
2018-06-06 18:11:53 +0800

    dev

--- server/controllers/demo.js
+++ server/controllers/demo.js
@@ -1,11 +1,7 @@
 
 const {mysql}=require('../qcloud')
 module.exports = async ctx => {
-
-  var imageInfo = {
-    imgUrl: "sdjsijdiwsj"
+  ctx.state.data = {
+    msg:'hello world'
   }
-  await mysql("album").insert(imageInfo)
-  var res = await mysql("album").where('id', 1)
-  ctx.state.data = res
 }
