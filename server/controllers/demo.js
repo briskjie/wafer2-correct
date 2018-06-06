935f7eb6f57fe74fc2d39eed776667dd69be08eb
xiangxuchen <xiangxuchen@sohu-inc.com>
2018-06-06 17:33:14 +0800

    dev

--- server/controllers/demo.js
+++ server/controllers/demo.js
@@ -1,5 +1,11 @@
-module.exports = ctx => {
-  ctx.state.data = {
-    msg: 'hello world'
+
+const {mysql}=require('../qcloud')
+module.exports = async ctx => {
+
+  var imageInfo = {
+    imgUrl: "sdjsijdiwsj"
   }
+  await mysql("album").insert(imageInfo)
+  var res = await mysql("album").where('id', 1)
+  ctx.state.data = res
 }
--- server/controllers/upload.js
+++ server/controllers/upload.js
@@ -1,9 +1,17 @@
+const { mysql } = require('../qcloud')
 const { uploader } = require('../qcloud')
 
 module.exports = async ctx => {
-    // 获取上传之后的结果
-    // 具体可以查看：
-    const data = await uploader(ctx.req)
-
-    ctx.state.data = data
+  // 获取上传之后的结果
+  // 具体可以查看：
+  const data = await uploader(ctx.req)
+  var imageInfo = {
+    imgUrl: '',
+    imgKey: ''
+  }
+  imageInfo.imgUrl = data.imgUrl
+  imageInfo.imgKey = data.imgKey
+  await mysql("album").insert(imageInfo)//将上传图片保存到数据库
+  // var res = await mysql("album").where('id', 1)
+  ctx.state.data = data
 }
